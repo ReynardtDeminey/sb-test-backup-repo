5d055152ce9e04413e4081cb108d4d5a28737cec
Reynardt Deminey <demineydev@gmail.com>
2020-02-18 17:28:25 +0200

    committing changes

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,6 +4,7 @@ import useBlogData from "../static_queries/useBlogData"
 import blogListStyles from "../styles/components/bloglist.module.scss"
 import Img from 'gatsby-image'
 
+
 export default function BlogList(props) {
   const blogData = useBlogData()
   function renderBlogData() {
@@ -14,6 +15,14 @@ export default function BlogList(props) {
           .map(blog => {
             return (              
                 <li className={blogListStyles.li}>
+                  <div className={blogListStyles.list__hero}>
+                    <img 
+                      src={
+                        props.src
+                      }
+                      alt={blog.node.frontmatter.title}
+                    />
+                    </div>
                   <div className={blogListStyles.list__info}>
                     <h2>{props.name}</h2>
                     <p>{props.description}</p>
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,17 +3,15 @@ import Layout from "../components/Layout"
 // import infoStyles from "../styles/pages/info.module.scss"
 // import useSiteMetaData from "../static_queries/useSiteMetadata"
 import ProjectList from "../components/ProjectList"
+import image1 from "../images/image1.jpg"
 
 export default function Projects(props) {
   
   return (
     <Layout page="projects" bgColor="#c7d1d9">
       <section>
-        <ProjectList name="Reddit Search Engine" description="A search engine built on the the pushshift API"/>
-        <ProjectList name="Reddit Search Engine" description="A search engine built on the the pushshift API"/>
-        <ProjectList name="Reddit Search Engine" description="A search engine built on the the pushshift API"/>
-        <ProjectList name="Reddit Search Engine" description="A search engine built on the the pushshift API"/>
-        <ProjectList name="Reddit Search Engine" description="A search engine built on the the pushshift API"/>
+        <ProjectList name="Reddit Search Engine" description="A search engine built on the the pushshift API" src={image1}/>
+        
       </section>
     </Layout>
   )
